29719fdfab9990bb611d8235292469e67031fad2
Gualtiero <gualtiero.frigerio@gmail.com>
2021-07-29 10:11:03 +0200

    empty test

--- Tests/GFImageViewTests/GFImageViewTests.swift
+++ Tests/GFImageViewTests/GFImageViewTests.swift
@@ -6,6 +6,6 @@ final class GFImageViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(GFImageView().text, "Hello, World!")
+        //XCTAssertEqual(GFImageView().text, "Hello, World!")
     }
 }

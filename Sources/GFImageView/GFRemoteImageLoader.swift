14fb22c29b5c90eaaaa0fe3711d34d8c9077dbab
Gualtiero <gualtiero.frigerio@gmail.com>
2021-08-02 09:00:16 +0200

    made public

--- Sources/GFImageView/GFRemoteImageLoader.swift
+++ Sources/GFImageView/GFRemoteImageLoader.swift
@@ -8,7 +8,7 @@ import Combine
 import Foundation
 
 @available(iOS 13.0, *)
-class RemoteImageLoader: ObservableObject {
+public class RemoteImageLoader: ObservableObject {
     var didChange = PassthroughSubject<Data, Never>()
     var data = Data() {
         didSet {
